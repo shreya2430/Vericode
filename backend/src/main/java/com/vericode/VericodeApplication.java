8978f5595c5ecddc4a0bc9a654e4f1218c641690
Shreya Wanisha <wanisha.s@northeastern.edu>
2026-03-18 01:20:09 -0400

    Remove tracked files now in .gitignore

---
+++ backend/src/main/java/com/vericode/VericodeApplication.java
@@ -0,0 +1,12 @@
+package com.vericode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VericodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VericodeApplication.class, args);
+	}
+}
